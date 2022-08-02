6404b403542763d9729883dcfad72285151aa009
baristunar <tunarbaris7@gmail.com>
2022-08-03 00:46:37 +0300

    Add: env example and set baseurl from env

--- src/utils/axios-instance.js
+++ src/utils/axios-instance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const AxiosInstance = () => {
   const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api/',
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
